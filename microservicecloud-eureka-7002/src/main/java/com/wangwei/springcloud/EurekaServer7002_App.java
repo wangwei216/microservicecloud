a5bcb31c964e1662202c43d581920a1c56540e91
wangwei216 <wangwei216@nou.com.cn>
2019-01-12 20:53:11 +0800

    集群高可用的eureka服务搭建成功，测试有点小问题7001可用

---
+++ microservicecloud-eureka-7002/src/main/java/com/wangwei/springcloud/EurekaServer7002_App.java
@@ -0,0 +1,12 @@
+package com.wangwei.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServer7002_App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer7002_App.class,args);
+    }
+}
---
+++ microservicecloud-eureka-7003/src/main/java/com/wangwei/springcloud/EurekaServer7003_App.java
@@ -0,0 +1,12 @@
+package com.wangwei.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServer7003_App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer7003_App.class,args);
+    }
+}

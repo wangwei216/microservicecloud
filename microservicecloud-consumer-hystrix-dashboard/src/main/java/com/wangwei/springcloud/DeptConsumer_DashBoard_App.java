c2d1acece6aac60ebc96be058d65c021c40a919a
wangwei216 <wangwei216@nou.com.cn>
2019-01-15 21:55:55 +0800

    整合Hystrix的dashboard服务监控，测试成功通过

---
+++ microservicecloud-consumer-hystrix-dashboard/src/main/java/com/wangwei/springcloud/DeptConsumer_DashBoard_App.java
@@ -0,0 +1,16 @@
+package com.wangwei.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class DeptConsumer_DashBoard_App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeptConsumer_DashBoard_App.class,args);
+
+    }
+}
